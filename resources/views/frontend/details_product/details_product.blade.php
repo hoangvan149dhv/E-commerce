634f840752cfee3d2b15a86f8a6ff893851e6b6a
Hoang Van <van.duong@redweb.dk>
2021-01-05 18:06:51 +0700

    template

--- resources/views/frontend/details_product/details_product.blade.php
+++ resources/views/frontend/details_product/details_product.blade.php
@@ -52,6 +52,14 @@
                             </a>
                         </div>
                     </div>
+                    <a class="carousel-control-prev" href="#product_details_slider" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#product_details_slider" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
             </div>
         </div>
